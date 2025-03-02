b5391c5ac4cdbe6ddd0ec59ceb27c473498ccfb5
Saman <samanmahmoudian.sm@gmail.com>
2025-03-02 17:23:37 +0330

    .

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-window.onload = async function() {
-    alert(window.Telegram.WebApp);
-}
+window.Telegram.WebApp.ready();
+
+setTimeout(() => {
+    if (window.Telegram.WebApp) {
+        console.log("✅ Telegram WebApp is available");
+    } else {
+        console.log("❌ Still not available");
+    }
+}, 1000);
