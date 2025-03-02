c2aaea3dcef04a04124a7c63379438b3f62ea03a
Saman <samanmahmoudian.sm@gmail.com>
2025-03-02 17:00:06 +0330

    .

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { peerConnectionConfig } from './PeerConfiguration.js';
+
 const myTelegramId = getTelegramId()
 let myId;
 let partnerId;
@@ -20,14 +20,66 @@ const nextBtn = document.getElementById('nextbtn');
 const startBtn = document.getElementById('startbtn');
 let playBtn = document.getElementById("playbutton");
 
+const peerConnectionConfig = {
+    iceServers: [
+        {
+            urls: "stun:stun.relay.metered.ca:80",
+        },
+        {
+            urls: "turn:global.relay.metered.ca:80",
+            username: "a4f5d501c33dfea6e2836653",
+            credential: "sxmhLRRVlHNc7aUL",
+        },
+        {
+            urls: "turn:global.relay.metered.ca:80?transport=tcp",
+            username: "a4f5d501c33dfea6e2836653",
+            credential: "sxmhLRRVlHNc7aUL",
+        },
+        {
+            urls: "turn:global.relay.metered.ca:443",
+            username: "a4f5d501c33dfea6e2836653",
+            credential: "sxmhLRRVlHNc7aUL",
+        },
+        {
+            urls: "turns:global.relay.metered.ca:443?transport=tcp",
+            username: "a4f5d501c33dfea6e2836653",
+            credential: "sxmhLRRVlHNc7aUL",
+        },
+        { urls: "stun:stun.l.google.com:19302" },
+        {
+            urls: "stun:stun.relay.metered.ca:80",
+        },
+        {
+            urls: "turn:global.relay.metered.ca:80",
+            username: "bff881b65e7cda72364ea616",
+            credential: "r93lIGVmjDZcQD5Y",
+        },
+        {
+            urls: "turn:global.relay.metered.ca:80?transport=tcp",
+            username: "bff881b65e7cda72364ea616",
+            credential: "r93lIGVmjDZcQD5Y",
+        },
+        {
+            urls: "turn:global.relay.metered.ca:443",
+            username: "bff881b65e7cda72364ea616",
+            credential: "r93lIGVmjDZcQD5Y",
+        },
+        {
+            urls: "turns:global.relay.metered.ca:443?transport=tcp",
+            username: "bff881b65e7cda72364ea616",
+            credential: "r93lIGVmjDZcQD5Y",
+        },
+    ],
+};
+
 
 async function getTelegramId(){
     await window.Telegram.WebApp.ready()
     if (window.Telegram.WebApp.initDataUnsafe) {
-        alert(window.Telegram.WebApp.initDataUnsafe.user.id)
+        nextBtn.innerText = window.Telegram.WebApp.initDataUnsafe.user.id;
         return window.Telegram.WebApp.initDataUnsafe.user.id;
     } else {
-        alert('Enter With Telegram')
+        nextBtn.innerText = 'Telegram not found';
         window.close()
     }
 }
