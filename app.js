27ccdf331a7d71e0318add45cad6043407ab5229
Saman <samanmahmoudian.sm@gmail.com>
2025-03-03 17:47:29 +0330

    .

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ async function getTelegramId(){
 }
 
 
-const socket = io(`https://miniapp-videocall-server.onrender.com` , {query: {userTelegramId: myTelegramId}});
+const socket = io(`http://localhost:3000` , {query: {userTelegramId: myTelegramId}});
 
 localstream.onplaying = function () {
     const loader = localstream.nextElementSibling;
