6b2c7aecb25d0162aa1a2343fff6baf337528888
Saman <samanmahmoudian.sm@gmail.com>
2025-03-03 20:45:38 +0330

    .

--- app.js
+++ app.js
@@ -167,13 +167,11 @@ async function createOffer(){
 }
 socket.on('caller' , async(partnerTelegramId)=>{
     partnerId = await partnerTelegramId
-    alert(partnerId)
     await createOffer()
 })
 
 socket.on('callee' , async(partnerTelegramId)=>{
     partnerId = await partnerTelegramId
-    alert(partnerId)
 })
 
 socket.on('offer' , async(offer)=>{
