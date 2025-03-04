c1ce2b2bb06eb7df1d5a33ba5c3d35ed506ff7d6
Saman <samanmahmoudian.sm@gmail.com>
2025-03-04 15:11:55 +0330

    .

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
+const peerConnectionConfig = {
+    iceServers: [
+        {
+          urls: "stun:stun.relay.metered.ca:80",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80",
+          username: "3d4c3bafb3a7da4b33bd3f07",
+          credential: "Ib6+qiOHo648ZsE5",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:80?transport=tcp",
+          username: "3d4c3bafb3a7da4b33bd3f07",
+          credential: "Ib6+qiOHo648ZsE5",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:443",
+          username: "3d4c3bafb3a7da4b33bd3f07",
+          credential: "Ib6+qiOHo648ZsE5",
+        },
+        {
+          urls: "turns:global.relay.metered.ca:443?transport=tcp",
+          username: "3d4c3bafb3a7da4b33bd3f07",
+          credential: "Ib6+qiOHo648ZsE5",
+        },
+    ],
+  }
 
+  
 const myTelegramId = String(Math.floor(Math.random() * 1000) + 1);
 alert(myTelegramId)
 let myId; 
@@ -36,33 +64,7 @@ remotestream.onplaying = function () {
     }
 };
 
-const peerConnectionConfig = {
-    iceServers: [
-        {
-          urls: "stun:stun.relay.metered.ca:80",
-        },
-        {
-          urls: "turn:global.relay.metered.ca:80",
-          username: "3d4c3bafb3a7da4b33bd3f07",
-          credential: "Ib6+qiOHo648ZsE5",
-        },
-        {
-          urls: "turn:global.relay.metered.ca:80?transport=tcp",
-          username: "3d4c3bafb3a7da4b33bd3f07",
-          credential: "Ib6+qiOHo648ZsE5",
-        },
-        {
-          urls: "turn:global.relay.metered.ca:443",
-          username: "3d4c3bafb3a7da4b33bd3f07",
-          credential: "Ib6+qiOHo648ZsE5",
-        },
-        {
-          urls: "turns:global.relay.metered.ca:443?transport=tcp",
-          username: "3d4c3bafb3a7da4b33bd3f07",
-          credential: "Ib6+qiOHo648ZsE5",
-        },
-    ],
-  }
+
 
 
 //  function getTelegramId(){
